5a64802c46b41627cdef59c1e9d58ddab17d76e1
Farooq Mahmud <farooq_am@hotmail.com>
2019-04-23 14:59:16 -0700

    Get Models for Make endpoint.

--- http-trigger-func/shared/carService.js
+++ http-trigger-func/shared/carService.js
@@ -2,11 +2,20 @@ const _ = require('lodash');
 
 const models = [
     { make: 'Ford', name: 'Edge', year: 2019 },
+    { make: 'Ford', name: 'Explorer', year: 2019 },
     { make: 'BMW', name: 'X7', year: 2019 },
     { make: 'Audi', name: 'S7', year: 2019 },
 ];
 
 const getModels = () => models;
+
+const getModelsForMake = (make) => {
+    if (!make) {
+        throw new Error('A make is required.');
+    }
+
+    return _.filter(models, m => (m.make.toLowerCase() === make.toLowerCase()));
+};
 
 const getModel = (make, name) => {
     if (!make || !name) {
@@ -19,5 +28,6 @@ const getModel = (make, name) => {
 
 module.exports = {
     getModels,
+    getModelsForMake,
     getModel,
 };
